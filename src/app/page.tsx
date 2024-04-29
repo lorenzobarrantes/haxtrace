997738c394adeb9f9bd641089a3f114cdf8553be
lorenzobarrantes <lorenzobarrantes1@gmail.com>
2024-04-29 18:02:17 -0300

    haxtrace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-screen overflow-x-hidden flex-col items-center justify-center p-24">
+    <main className="flex h-screen max-w-screen overflow-x-hidden flex-col items-center justify-center p-24">
       <div className="z-10 w-full top-0 absolute mt-8 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <a href='https://quadrant.com.ar' className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Volver
@@ -26,14 +26,14 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="flex justify-center items-center flex-col">
-      <div className=" z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-red-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-[#c23A00] after:dark:via-[#bd0014] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="flex justify-center items-center flex-col w-full mx-2">
+      <div className=" z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full md:after:translate-x-1/3 after:-translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-red-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-[#c23A00] after:dark:via-[#bd0014] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#202020] mb-4"
           src="/HT.png"
           alt="HaxTrace Logo"
-          width={480}
-          height={37}
+          width={580}
+          height={137}
           priority
         />
       </div>
